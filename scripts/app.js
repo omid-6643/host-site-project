50500ed77417451a54e0a64eaf51c321148dbe1a
omid beigi <o.beygi@gmail.com>
2022-04-29 14:53:04 +0430

    4/29

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+let icon = document.querySelector(".menu-icon");
+let menu = document.querySelector(".main-menu");
+
+icon.addEventListener("click", function () {
+  if (icon.classList.contains("fa-bars")) {
+    menu.style.left = 0;
+    icon.classList = "fa fa-times menu-icon";
+  } else {
+    menu.style.left = "-250px";
+    icon.classList = "fa fa-bars menu-icon";
+  }
+  //   if (icon.classList.contains("fa-times")) {
+  //     menu.style.left = '-250px';
+  //   }
+});
